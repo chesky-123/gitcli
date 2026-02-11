1f45009a3ab3b4081ec72c2d07ff3183acc5e389
chesky <sye0534184736@gmail.com>
2026-02-11 15:10:52 +0200

    add a func get list

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-h=7
-kfkk=9
-g=8
+from pathlib import Path
+
+def get_list_of_file(path):
+    with open(path,'r') as file:
+        list_of_lines=[line for line in [w.split(",") for w in file.readlines()]]
+    return list_of_lines
+
+
+
+
+
+
+
+
+
