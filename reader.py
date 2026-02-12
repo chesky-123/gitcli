cfadbfef40fb3ad718f5c5fb1dd1ba0690915115
chesky <sye0534184736@gmail.com>
2026-02-12 16:21:08 +0200

    add a func nighttime_activity_filtering

--- reader.py
+++ reader.py
@@ -8,4 +8,6 @@ convert_from_byte_to_kilobyte=list(map(lambda row:int(row[-1])/1024, list_of_pys
 
 filter_rows_by_port=list(filter(lambda line:line[3]=="22" or line[3]=="23" or line[3]=="3389",list_of_pys))
 
+nighttime_activity_filtering=list(filter(lambda n:"00:00:00"<= n[0][11:19] <= "00:06:00",list_of_pys))
+
 
