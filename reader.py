9e5ccf9a01fe3afcc230550a099cfe64ce9be7aa
chesky <sye0534184736@gmail.com>
2026-02-12 16:04:37 +0200

    add a func thet convert_from_byte_to_kilobyte

--- reader.py
+++ reader.py
@@ -4,3 +4,7 @@ list_of_pis=get_list_of_file("network_traffic.log")
 
 get_the_hour=list(map(lambda time:int(time[0][11:13]),list_of_pis))
 
+convert_from_byte_to_kilobyte=list(map(lambda row:int(row[-1])/1024,list_of_pis))
+
+
+
