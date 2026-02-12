5da6a0022c060fbbe8dda2727fbc7e13e83a9aa8
chesky <sye0534184736@gmail.com>
2026-02-12 14:27:56 +0200

    hh

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 
 def get_list_of_file(path):
     with open(path,'r') as file:
@@ -7,9 +6,19 @@ def get_list_of_file(path):
 
 
 def extracting_external_IP_addresses(lst:list):
-    list_external_addresses=[l for l in lst if not l[1].startswith("192.168") if not l[1].startswith("10.")]
+    list_external_addresses=[l[1] for l in lst if not l[1].strip().startswith("192.168") and not l[1].strip().startswith("10.")]
     return list_external_addresses
 
 
+def filter_by_sensitive_port(lst:list):
+    sensitive = ["22", "23", "3389"]
+    list_filtered_by_sensitive_port=[l for l in lst if l[3].strip() in sensitive]
+    return list_filtered_by_sensitive_port
 
+def filter_by_size(lst:list):
+    list_filter_by_size=list(filter(lambda l:int(l[-1])>5000,lst))
+    return list_filter_by_size
 
+def traffic_tagging(lst:list):
+    updated_list=["LARGE" if int(row[-1])>5000  else "NORMAL" for row in lst]
+    return updated_list
