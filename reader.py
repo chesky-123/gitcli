502c96b3cbc25f73aa5d33ccd92e2c24f6e52912
chesky <sye0534184736@gmail.com>
2026-02-12 16:16:04 +0200

    add a func filter_rows_by_port

--- reader.py
+++ reader.py
@@ -1,10 +1,11 @@
 from config import *
 
-list_of_pis=get_list_of_file("network_traffic.log")
+list_of_pys=get_list_of_file("network_traffic.log")
 
-get_the_hour=list(map(lambda time:int(time[0][11:13]),list_of_pis))
+get_the_hour=list(map(lambda time:int(time[0][11:13]), list_of_pys))
 
-convert_from_byte_to_kilobyte=list(map(lambda row:int(row[-1])/1024,list_of_pis))
+convert_from_byte_to_kilobyte=list(map(lambda row:int(row[-1])/1024, list_of_pys))
 
+filter_rows_by_port=list(filter(lambda line:line[3]=="22" or line[3]=="23" or line[3]=="3389",list_of_pys))
 
 
